c1b54a0fa5ff82682c71be05c16f0370c9a017e0
Raul Henrique Domingues <raulhdomingues@hotmail.com>
2023-11-27 16:46:27 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './componentes/Header'
-import Pesquisa from './componentes/Pesquisa'
 import styled from 'styled-components'
+import Pesquisa from './componentes/Pesquisa'
+import UltimosLancamentos from './componentes/UltimosLancamentos'
 
 const AppContainer = styled.div`
     width: 100vw;
@@ -18,6 +19,7 @@ function App() {
     <AppContainer>
       <Header/>
       <Pesquisa />
+      <UltimosLancamentos />
     </AppContainer>
   );
 }
